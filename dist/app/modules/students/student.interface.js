89c812bbef68b5ae34fab7d5289b029215050dae
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-02-13 00:52:41 +0600

    Updated .gitignore

--- dist/app/modules/students/student.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// for creating instance 
-// export type StudentMethod = {
-//   isUserExists(id: string): Promise<TStudent | null>;
-// };
-// export type StudentModel = Model<TStudent, {}, StudentMethod>;
